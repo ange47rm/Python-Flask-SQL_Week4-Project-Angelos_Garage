6609ed1c258019a2e3f46cf62d7b6e07ac6dd6d3
ange47rm <ange47rm@hotmail.com>
2020-12-12 13:06:55 +0000

    Manufacturer 4 tests passed. Moving onto Car class tests.

--- tests/manufacturer_test.py
+++ tests/manufacturer_test.py
@@ -13,3 +13,12 @@ class TestManufacturer (unittest.TestCase):
 
     def test_manufacturer_has_name (self):
         self.assertEqual ('Mazda', self.manufacturer_1.name)
+
+    def test_manufacturer_has_phone_no (self):
+        self.assertEqual ('0123 451 749', self.manufacturer_2.phone)
+
+    def test_manufacturer_has_email (self):
+        self.assertEqual ('email@nissan.com', self.manufacturer_3.email)
+
+    def test_manufacturer_has_website (self):
+        self.assertEqual ('www.subaru.com', self.manufacturer_4.website)
